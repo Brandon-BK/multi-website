80ae5f85c297f141873e6045e9fc5948155f63d6
Brandon-BK <activemedia@activemi.co.za>
2023-07-11 10:51:59 +0200

    template

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="App">
+      {/* Please import all your components here and in order navbars first, followed by banners etc... */}
+
+    </div>
+  );
+}
+
+export default App;
